c9600f9fc031f4429d68e6da285add6daa01b24c
gigiblender <swenky_4u@yahoo.com>
2014-08-14 15:25:40 +0300

    bug solved and added buttons "animation"
    proportion of the buttons textures solved and added kind of an animation
    when tapping any button

--- core/src/com/hubclub/color_madness/RetryScreen.java
+++ core/src/com/hubclub/color_madness/RetryScreen.java
@@ -1,6 +1,7 @@
 package com.hubclub.color_madness;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
@@ -8,10 +9,9 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Rectangle;
 
-public class RetryScreen implements Screen{
+public class RetryScreen implements Screen,InputProcessor{
 
 	private ColorGame game;
 	private BitmapFont font;
@@ -21,7 +21,7 @@ public class RetryScreen implements Screen{
 	private FileHandle file;
 	private boolean hard;
 	private int score;
-
+	private boolean retryTouched,menuTouched;
     private Texture background;
     private Texture retryButton;
     private Texture backButton;
@@ -35,10 +35,10 @@ public class RetryScreen implements Screen{
 	public void set(boolean hard,int score){
 		this.score=score;
 		shape = new ShapeRenderer();
-
+	
 		retry = new Rectangle(Gdx.graphics.getWidth() / 2 - Constants.RETRY_WIDTH / 2, 500*Constants.height, Constants.RETRY_WIDTH, Constants.RETRY_HEIGHT);
 		menu = new Rectangle(Gdx.graphics.getWidth() / 2 - Constants.BACK_WIDTH / 2, 300*Constants.height, Constants.BACK_WIDTH, Constants.BACK_HEIGHT);
-
+		Gdx.input.setInputProcessor(this);
 		pointer = new Rectangle(0,0,0.1f, 0.1f);
 		batch=new SpriteBatch();
 		font=new BitmapFont();
@@ -75,26 +75,8 @@ public class RetryScreen implements Screen{
 		font.draw(batch, "Your highscore is: " +file.readString(), 0, 50*Constants.height);
 		batch.end();
 
-		if (Gdx.input.justTouched()) {
-			pointer.x=Gdx.input.getX();
-			pointer.y=800*Constants.height - Gdx.input.getY();
-			if (pointer.overlaps(menu)) {
-				this.dispose();
-				ColorGame.menuScreen.set();
-				game.setScreen(ColorGame.menuScreen);
+}
 
-			}
-
-			if (pointer.overlaps(retry)) {
-				this.dispose();
-				ColorGame.mainScreen.set(2,hard,0);
-				game.setScreen(ColorGame.mainScreen);
-
-			}
-
-		}
-
-	}
 
 	@Override
 	public void resize(int width, int height) {
@@ -136,6 +118,89 @@ public class RetryScreen implements Screen{
 
 		// TODO Auto-generated method stub
 		//game.dispose();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		this.pointer.x=screenX;
+		this.pointer.y=800*Constants.height-screenY;
+		if(this.pointer.overlaps(menu) && !retryTouched){
+			backButton.dispose();
+			backButton=new Texture("back1.png");
+			menu.x-=2;
+			menu.y-=2;
+			menuTouched=true;
+		}
+		if(this.pointer.overlaps(retry) && !menuTouched){
+			retryButton.dispose();
+			retryButton=new Texture("retry1.png");
+			retry.x-=2;
+			retry.y-=2;
+			retryTouched=true;
+		}
+		//System.out.println(button);
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		if(retryTouched){
+			this.dispose();
+			retryButton.dispose();
+			backButton.dispose();
+			retryTouched=false;
+			ColorGame.mainScreen.set(2,hard,0);
+			game.setScreen(ColorGame.mainScreen);
+			
+
+		}
+		if(menuTouched){
+			this.dispose();
+			retryButton.dispose();
+			backButton.dispose();
+			menuTouched=false;
+			ColorGame.menuScreen.set();
+			game.setScreen(ColorGame.menuScreen);
+		}
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 
